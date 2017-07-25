f5de3ea9ff8e0f1ba694f13eba1898f5b68664a1
mix6s <mix6s@yandex.ru>
2017-07-25 23:21:43 +0300

    Add Test

---
+++ src/Domain/Tests/UseCase/CreateTeamUseCaseTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simple
+ * Date: 25.07.2017
+ * Time: 22:40
+ */
+
+namespace Domain\Tests\UseCase;
+
+use Domain\UseCase\CreateTeamUseCase;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CreateTeamUseCaseTest
+ * @package Domain\Tests\UseCase
+ */
+class CreateTeamUseCaseTest extends TestCase
+{
+
+}
