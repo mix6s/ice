f1984a00191b0ec6a7d84a1956386b3b3bf8509e
mix6s <mix6s@yandex.ru>
2017-07-27 23:23:06 +0300

    rename admin routes

--- src/AppBundle/Controller/ControlController.php
+++ src/AppBundle/Controller/ControlController.php
@@ -15,7 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 /**
  * Class ControlController
  * @package AppBundle\Controller
- * @Route("/backend")
+ * @Route("/control")
  */
 class ControlController extends Controller
 {
